54fea22dd80439a75026b7756ac5914eb21091e4
Sonja Künzl <92093306+sonjaknzl@users.noreply.github.com>
2025-10-14 10:21:20 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,56 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
-import TaskForm from './components/TaskForm.vue'
-import type { Task, TaskFilter } from './types'
-import TaskList from './components/TaskList.vue'
-import FilterButton from './components/FilterButton.vue'
+import { computed, ref } from "vue";
+import TaskForm from "./components/TaskForm.vue";
+import type { Task, TaskFilter } from "./types";
+import TaskList from "./components/TaskList.vue";
+import FilterButton from "./components/FilterButton.vue";
 
-const message = ref('Tasks App')
-const tasks = ref<Task[]>([])
-const filter = ref<TaskFilter>('all')
+const message = ref("Tasks App");
+const tasks = ref<Task[]>([]);
+const filter = ref<TaskFilter>("all");
 
 const totalDone = computed(() =>
   tasks.value.reduce((total, task) => (task.done ? total + 1 : total), 0)
-)
+);
 
 const filteredTasks = computed(() => {
   switch (filter.value) {
-    case 'all':
-      return tasks.value
-    case 'done':
-      return tasks.value.filter((task) => task.done)
-    case 'todo':
-      return tasks.value.filter((task) => !task.done)
+    case "all":
+      return tasks.value;
+    case "done":
+      return tasks.value.filter((task) => task.done);
+    case "todo":
+      return tasks.value.filter((task) => !task.done);
     default:
-      return tasks.value
+      return tasks.value;
   }
-})
+});
 
 const addTask = (newTask: string) => {
   tasks.value.push({
     id: crypto.randomUUID(),
     title: newTask,
     done: false,
-  })
-}
+  });
+};
 
 const toggleDone = (id: string) => {
-  const task = tasks.value.find((task) => task.id === id)
+  const task = tasks.value.find((task) => task.id === id);
   if (task) {
-    task.done = !task.done
+    task.done = !task.done;
   }
-}
+};
 
 const removeTask = (id: string) => {
-  const index = tasks.value.findIndex((task) => task.id === id)
+  const index = tasks.value.findIndex((task) => task.id === id);
   if (index !== -1) {
-    tasks.value.splice(index, 1)
+    tasks.value.splice(index, 1);
   }
-}
+};
 
 const setFilter = (value: TaskFilter) => {
-  filter.value = value
-}
+  filter.value = value;
+};
 </script>
 
 <template>
@@ -60,11 +60,27 @@ const setFilter = (value: TaskFilter) => {
     <h3 v-if="!tasks.length">Add a task to get started</h3>
     <h3 v-else>{{ totalDone }} / {{ tasks.length }} tasks completed</h3>
     <div v-if="tasks.length" class="button-container">
-      <FilterButton :currentFilter="filter" filter="all" @set-filter="setFilter" />
-      <FilterButton :currentFilter="filter" filter="todo" @set-filter="setFilter" />
-      <FilterButton :currentFilter="filter" filter="done" @set-filter="setFilter" />
+      <FilterButton
+        :currentFilter="filter"
+        filter="all"
+        @set-filter="setFilter"
+      />
+      <FilterButton
+        :currentFilter="filter"
+        filter="todo"
+        @set-filter="setFilter"
+      />
+      <FilterButton
+        :currentFilter="filter"
+        filter="done"
+        @set-filter="setFilter"
+      />
     </div>
-    <TaskList :tasks="filteredTasks" @toggle-done="toggleDone" @remove-task="removeTask" />
+    <TaskList
+      :tasks="filteredTasks"
+      @toggle-done="toggleDone"
+      @remove-task="removeTask"
+    />
   </main>
 </template>
 
